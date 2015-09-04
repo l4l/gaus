5f253baf474ad2a2ed729bd9ae12bf0011b09377
Kitsu <mail@kitsu.me>
2015-09-04 11:32:08 +0300

    Added threaded for common function, set image filtering by line, not by pixel

--- src/threader.py
+++ src/threader.py
@@ -3,21 +3,38 @@ import threading
 __author__ = 'kitsu'
 
 
+class LineThreader (threading.Thread):
+    threads = 0
+    id = 0
+
+    def __init__(self, fun, buf, width: int, y: int):
+        threading.Thread.__init__(self)
+        LineThreader.id += 1
+        self.id = LineThreader.id
+        self.fun = fun
+        self.buf = buf
+        self.width = width
+        self.y = y
+
+    def run(self):
+        # print(self.id)
+        LineThreader.threads += 1
+        for x in range(0, self.width):
+            self.buf[(x, self.y)] = self.fun(x)
+        LineThreader.threads -= 1
+
+
 class Threader (threading.Thread):
     threads = 0
     id = 0
 
-    def __init__(self, fun, x: int, y: int, buf):
+    def __init__(self, fun):
         threading.Thread.__init__(self)
-        Threader.id += 1
-        self.id = Threader.id
-        self.x = x
-        self.y = y
-        self.buf = buf
+        LineThreader.id += 1
+        self.id = LineThreader.id
         self.fun = fun
 
     def run(self):
-        # print(self.id)
-        Threader.threads += 1
-        self.buf[(self.x, self.y)] = self.fun()
-        Threader.threads -= 1
+        LineThreader.threads += 1
+        self.fun()
+        LineThreader.threads -= 1
